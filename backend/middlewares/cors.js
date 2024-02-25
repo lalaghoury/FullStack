d8b2ce3fd2ab665b1a108bf07b7e6b1388f6e6ad
lalaghoury <127485169+lalaghoury@users.noreply.github.com>
2024-02-25 23:45:31 +0500

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://frontend-two-red-62.vercel.app",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })

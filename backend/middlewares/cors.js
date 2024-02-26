96a712d6027043127ee562dddc365f16a2119bb1
lalaghoury <127485169+lalaghoury@users.noreply.github.com>
2024-02-26 09:35:55 +0500

    Update cors.js

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const useCors = (app) => {
   app.use(
     cors({
-      origin: "https://frontend-moy6x4t1n-aasil-ghourys-projects.vercel.app",
+      origin: "https://perfectrecipebyaasil.vercel.app",
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     })
